53befe90d0cff63d37e303624d0da8eeb2938679
Mohamed <mohamedsalah3072001@gmail.com>
2024-01-23 21:04:23 +0200

    Mohamed Salah: Splash and Login.

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class SignUp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Sign Up",
+          style: TextStyle(
+              fontSize: 35,
+              fontWeight: FontWeight.bold,
+              color: Colors.red
+          ),
+        ),
+      ),
+    );
   }
 
 }
