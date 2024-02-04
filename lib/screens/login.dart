df8f7e81d11dcc01ca97bb079ceab2ce78963af3
Mohamed Alaa <malaa3213@gmail.com>
2024-02-04 04:01:01 -0800

    mohamed alaa internet connection and favourites 1

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -41,105 +41,107 @@ class login extends StatelessWidget {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: emailController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  labelText: "E-mail",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide:
-                        BorderSide(color: Color.fromRGBO(36, 54, 101, 1.0)),
-                  )),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                  focusedBorder: OutlineInputBorder(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    labelText: "E-mail",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
                       borderSide:
-                          BorderSide(color: Color.fromRGBO(36, 54, 101, 1.0)))),
-              obscureText: true,
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Center(
-              child: ElevatedButton(
-                  onPressed: () async {
-                    if (emailController.text.isNotEmpty &&
-                        RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(emailController.text) &&
-                        passwordController.text.isNotEmpty) {
-                      try {
-                        await _auth.signInWithEmailAndPassword(
-                            email: emailController.text,
-                            password: passwordController.text);
-                      } catch (e) {
-                        _showToast('Wrong Email and Password');
+                          BorderSide(color: Color.fromRGBO(36, 54, 101, 1.0)),
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                    labelText: "Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Color.fromRGBO(36, 54, 101, 1.0)))),
+                obscureText: true,
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: ElevatedButton(
+                    onPressed: () async {
+                      if (emailController.text.isNotEmpty &&
+                          RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(emailController.text) &&
+                          passwordController.text.isNotEmpty) {
+                        try {
+                          await _auth.signInWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passwordController.text);
+                        } catch (e) {
+                          _showToast('Wrong Email and Password');
+                        }
+                        if (_auth.currentUser != null) {
+                          Get.offAll(() => bottomNavigation());
+                        }
+                      } else {
+                        _showToast("Please enter E-mail and Password");
                       }
-                      if (_auth.currentUser != null) {
-                        Get.offAll(() => bottomNavigation());
-                      }
-                    } else {
-                      _showToast("Please enter E-mail and Password");
-                    }
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(
-                        Color.fromRGBO(36, 54, 101, 1.0)),
-                  ),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  )),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "Do not have an account?",
-                  style: TextStyle(fontSize: 20),
-                ),
-                TextButton(
-                    onPressed: () {
-                      Get.to(() => SignUp());
                     },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                          Color.fromRGBO(36, 54, 101, 1.0)),
+                    ),
                     child: Text(
-                      "Sign Up",
+                      "Login",
                       style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          decorationColor: Color.fromRGBO(36, 54, 101, 1.0),
-                          decorationThickness: 2,
-                          color: Color.fromRGBO(36, 54, 101, 1.0),
-                          fontSize: 20),
-                    ))
-              ],
-            ),
-            SizedBox(height: 10),
-            Obx(() => connectivityController.isConnected.value ?Text(''):Center(child: Text('No Internet Connection',style: TextStyle(color: Colors.red),)))
-          ],
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    )),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "Do not have an account?",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Get.to(() => SignUp());
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            decorationColor: Color.fromRGBO(36, 54, 101, 1.0),
+                            decorationThickness: 2,
+                            color: Color.fromRGBO(36, 54, 101, 1.0),
+                            fontSize: 20),
+                      ))
+                ],
+              ),
+              SizedBox(height: 10),
+              Obx(() => connectivityController.isConnected.value ?Text(''):Center(child: Text('No Internet Connection',style: TextStyle(color: Colors.red),)))
+            ],
+          ),
         ),
       ),
     );
