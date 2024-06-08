a3b1878f9137e48096b9c949bf1a3279c356efdd
Mohamed <mohamedsalah3072001@gmail.com>
2024-06-09 00:22:46 +0300

    Cars Store

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -9,7 +9,7 @@ import '../models/post_model.dart';
 class home extends StatelessWidget {
   final FirestoreController firestoreController =
       Get.put(FirestoreController());
-  final TextEditingController _searchController = TextEditingController();
+
   List<PostModel>? posts;
 
   @override
@@ -24,137 +24,115 @@ class home extends StatelessWidget {
               color: Color.fromRGBO(36, 54, 101, 1.0)),
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              controller: _searchController,
-              decoration: InputDecoration(
-                hintText: 'Search for cars by brand',
-                prefixIcon: const Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              onChanged: (value) {
-                // Perform search when the text in the search bar changes
-                _performSearch(value);
-              },
-            ),
-          ),
-          Obx(() => firestoreController.posts.isEmpty
-              ? const Center(child: CircularProgressIndicator())
-              : Expanded(
-                child: RefreshIndicator(
-                    onRefresh: () {
-                      return firestoreController.getPosts();
-                    },
-                    child: SingleChildScrollView(
-                      physics: const AlwaysScrollableScrollPhysics(),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          const Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 10),
-                            child: Text(
-                              'Posts',
-                              style: TextStyle(
-                                  fontSize: 20, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Obx(() => ListView.builder(
-                              shrinkWrap: true,
-                              physics: const NeverScrollableScrollPhysics(),
-                              itemCount: firestoreController.posts.length,
-                              itemBuilder: (context, index) {
-                                return GestureDetector(
-                                  onTap: () {
-                                    Get.to(() => ShowDetails(
-                                        firestoreController.posts[index]));
-                                  },
-                                  child: Card(
-                                    color: Colors.white,
-                                    margin: const EdgeInsets.symmetric(
-                                        vertical: 8, horizontal: 10),
-                                    child: Column(
-                                      children: [
-                                        ClipRRect(
-                                          borderRadius: BorderRadius.circular(12),
-                                          child: FadeInImage(
-                                              width: 400,
-                                              height: 200,
-                                              fit: BoxFit.cover,
-                                              image: NetworkImage(
-                                                firestoreController
-                                                    .posts[index].images![0],
-                                              ),
-                                              placeholder: const AssetImage(
-                                                  'assets/images/loading.gif')),
-                                        ),
-                                        ListTile(
-                                          contentPadding:
-                                              const EdgeInsets.fromLTRB(16, 0, 16, 0),
-                                          title: Align(
+      body: Obx(() => firestoreController.posts.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : Expanded(
+            child: RefreshIndicator(
+                onRefresh: () {
+                  return firestoreController.getPosts();
+                },
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'Posts',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Obx(() => ListView.builder(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemCount: firestoreController.posts.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Get.to(() => ShowDetails(
+                                    firestoreController.posts[index]));
+                              },
+                              child: Card(
+                                color: Colors.white,
+                                margin: const EdgeInsets.symmetric(
+                                    vertical: 8, horizontal: 10),
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(12),
+                                      child: FadeInImage(
+                                          width: 400,
+                                          height: 200,
+                                          fit: BoxFit.cover,
+                                          image: NetworkImage(
+                                            firestoreController
+                                                .posts[index].images![0],
+                                          ),
+                                          placeholder: const AssetImage(
+                                              'assets/images/loading.gif')),
+                                    ),
+                                    ListTile(
+                                      contentPadding:
+                                          const EdgeInsets.fromLTRB(16, 0, 16, 0),
+                                      title: Align(
+                                          alignment: Alignment.topLeft,
+                                          child: Text(
+                                            '${firestoreController.posts[index].brand} ${firestoreController.posts[index].model} ${firestoreController.posts[index].year}',
+                                            style: const TextStyle(
+                                                fontSize: 25,
+                                                fontWeight: FontWeight.bold),
+                                          )),
+                                      subtitle: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceAround,
+                                        children: [
+                                          Align(
                                               alignment: Alignment.topLeft,
                                               child: Text(
-                                                '${firestoreController.posts[index].brand} ${firestoreController.posts[index].model} ${firestoreController.posts[index].year}',
-                                                style: const TextStyle(
-                                                    fontSize: 25,
-                                                    fontWeight: FontWeight.bold),
-                                              )),
-                                          subtitle: Column(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceAround,
-                                            children: [
-                                              Align(
-                                                  alignment: Alignment.topLeft,
-                                                  child: Text(
-                                                      "EGP ${firestoreController.posts[index].price}",
-                                                      style: const TextStyle(
-                                                          fontSize: 16,
-                                                          fontWeight:
-                                                              FontWeight.w500))),
-                                              Align(
-                                                  alignment: Alignment.topLeft,
-                                                  child: Text(
+                                                  "EGP ${firestoreController.posts[index].price}",
+                                                  style: const TextStyle(
+                                                      fontSize: 16,
+                                                      fontWeight:
+                                                          FontWeight.w500))),
+                                          Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Text(
+                                                  firestoreController
+                                                      .posts[index].location
+                                                      .toString(),
+                                                  style: const TextStyle(
+                                                      fontSize: 16,
+                                                      fontWeight:
+                                                          FontWeight.w500))),
+                                          Align(
+                                              alignment: Alignment.topLeft,
+                                              child: Text(
+                                                  getComparedTime(
                                                       firestoreController
-                                                          .posts[index].location
-                                                          .toString(),
-                                                      style: const TextStyle(
-                                                          fontSize: 16,
-                                                          fontWeight:
-                                                              FontWeight.w500))),
-                                              Align(
-                                                  alignment: Alignment.topLeft,
-                                                  child: Text(
-                                                      getComparedTime(
-                                                          firestoreController
-                                                              .posts[index].time
-                                                              .toString()),
-                                                      style: const TextStyle(
-                                                          fontSize: 13,
-                                                          fontWeight: FontWeight.w500,
-                                                          color: Colors.grey)))
-                                            ],
-                                          ),
-                                        ),
-                                      ],
+                                                          .posts[index].time
+                                                          .toString()),
+                                                  style: const TextStyle(
+                                                      fontSize: 13,
+                                                      fontWeight: FontWeight.w500,
+                                                      color: Colors.grey)))
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                );
-                              })),
-                        ],
-                      ),
-                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          })),
+                    ],
                   ),
-              )),
-        ],
-      ),
+                ),
+              ),
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.to(() => AddPost());
@@ -168,16 +146,6 @@ class home extends StatelessWidget {
         child: const Icon(Icons.add),
       ),
     );
-  }
-
-  void _performSearch(String query) {
-    if (query.isEmpty) {
-      // If the query is empty, show all posts
-      firestoreController.getPosts();
-    } else {
-      // Filter the posts based on the search query
-      firestoreController.searchCarsByName(query);
-    }
   }
 
   String getComparedTime(String dateTime) {
